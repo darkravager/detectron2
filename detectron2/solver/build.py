54ba911b625da0271602e04e04d8b2a779791eec
darkravager <88234632+darkravager@users.noreply.github.com>
2021-11-25 00:40:37 +0530

    Update build.py

--- detectron2/solver/build.py
+++ detectron2/solver/build.py
@@ -122,11 +122,9 @@ def build_optimizer(cfg: CfgNode, model: torch.nn.Module) -> torch.optim.Optimiz
         bias_lr_factor=cfg.SOLVER.BIAS_LR_FACTOR,
         weight_decay_bias=cfg.SOLVER.WEIGHT_DECAY_BIAS,
     )
-    return maybe_add_gradient_clipping(cfg, torch.optim.SGD)(
+    return maybe_add_gradient_clipping(cfg, torch.optim.AdamW)(
         params,
         lr=cfg.SOLVER.BASE_LR,
-        momentum=cfg.SOLVER.MOMENTUM,
-        nesterov=cfg.SOLVER.NESTEROV,
         weight_decay=cfg.SOLVER.WEIGHT_DECAY,
     )
 
